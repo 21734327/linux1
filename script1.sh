1008560171ecb6b9726a90cc88f915fb2081c8df
21734327 <49047522+21734327@users.noreply.github.com>
2019-04-04 19:01:12 -0500

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+#name: script1
+#purpose: show user's login information
+#version: 1.0
+#created: 2019-04-04
+#creator nathanial krueger
+
+echo "Hello, $LOGNAME.  Today is `date +%A`, `date +%B`.  The time is `date +I:%M` `date +%P`."
+echo "You have logged into a ${TERM^} Terminal."
